431cb6bd7f30b927d414b3fb331c6c92d177910c
dianaokoro <dianaokoro09@gmail.com>
2022-04-11 16:39:46 +0100

    my js learn

---
+++ index.js
@@ -0,0 +1,214 @@
+// // let n = 'Diana';
+// // let x = 4;
+// // let b = true;
+// // let g = 5.983;
+
+// // let coworkers = ["Festus", "Victor", "Akin", "Ifunanya", "Daniel"]
+
+// // console.log(n);
+// // console.log(n);
+// // console.log(n);
+// // console.log(n);
+// // console.log(n);
+// // console.log(n);
+// // console.log(n);
+
+// let n = "Diana";
+
+// console.log(n);
+
+// n = "Kalu"
+
+// console.log(n)
+
+// console.log("100" + 1);
+
+
+// function addNumbers(x, y) {
+//     console.log(x + y)
+// }
+
+// addNumbers(5, 10)
+
+
+// // let str = 'Hello';
+// // let bool = true;
+// // console.log(str + bool);
+
+// // let surname = "Okoro"
+
+// // if(surname == "okoro"){
+// //     console.log("you be family")
+// // } else {
+// //     console.log("you no be family")
+// // }
+
+// if ("false"){
+//     console.log("Javascript na bitch")
+// }
+
+
+// function tellMyAgeIn100Years(age) {
+//     let newage = age + 100
+//     console.log(newage)
+// }
+
+
+// tellMyAgeIn100Years(40)
+// tellMyAgeIn100Years(50)
+// tellMyAgeIn100Years(100)
+//console.log(5 + 5 )
+
+// let num = 5  // number
+// console.log(num);
+
+// let user = 'Diana'  // string
+
+//let name = 'okoro diana\nezinne'
+//console.log(name)
+
+//let user = null
+//console.log(typeof user);
+
+// let num = 8
+// let result = 8 ** 8
+//  console.log(result);  
+
+
+   
+
+// // function addNum(x, y, z){
+// //     let sum = x + y + z
+// // }
+
+
+// // addNum(50, 100, 50)
+
+// let day = "Monday"
+// switch(day){
+//     case 'Monday':
+//         console.log("6am");
+//         break;
+//     case 'Tuesday':
+//         console.log("8am");
+//         default:
+//             console.log(stop); 
+    
+            
+        
+// }
+
+//         //template literal
+
+//  let num1 = 8
+//  //let num2 = 5
+//  //let result = num1 + num2
+ 
+//  //console.log(`The addition of ${num1} and ${num2} is ${result}`);
+
+//         //while loop
+//  // initialize, condition, increment
+  
+//  let i =1 ;
+
+//  while(i<=5)
+// {
+//  console.log("Hi");
+//  i++;
+// }
+      //do while
+ //let i = 1;
+
+  //do
+ //{
+     //console.log("hi",i);
+     //i++;
+ //}while (i<=5) {
+     
+ //}
+           // for loop
+      // let i=1;-------    
+  //  for(let i=1;i<=100;i++)
+  //{
+    // if(i%3===0) 
+    //  console.log(i);
+ // }  
+         
+
+        //for(let i = 0; i < 100; i++)
+        //{
+            //console.log("Diana")
+       // }
+
+       //Object--//
+  //let alien ={
+      //name: 'Diana',
+      //tech: 'Js'
+
+  //}   
+  
+  //console.log(alien);
+  ////you use the . operator, to fetch the property of an object///
+  //eg. 
+  //let alian ={
+      //name: 'diana',
+      //tech; 'js'
+     // console.log(alian.name);
+  //}
+//// a . operator can not be used if the property has two words eg, work exp.(this is called square braket) 
+//eg. 
+//name: 'Diana',
+//tech: 'js',
+//'work exp': 5
+//complex Object  // An object inside another object//
+
+// let alian ={
+//     name: 'Diana',
+//     tech: 'js',
+//     laptop: {
+//         cpu: 'i7',
+//         ram: 4,
+//         brand: 'hp'
+
+//     }
+// }
+// console.log(alian.laptop.brand?.length);
+
+
+////function
+// function greet() 
+// {
+//     console.log('Hello World'); 
+// }
+
+// greet();
+ ////return and passing function
+ /*function greet() 
+ {
+     return('Hello World'); 
+ }
+
+ let str = greet();
+ console.log(str );*/
+
+     ///passing
+ /*function greet(user)
+ {
+     return `hello ${user}`
+
+
+     let user = 'Diana';
+     let str = greet(user);
+     console.log(str);*/ 
+
+         //function expresion//
+     
+    /* let add = function (num1,num2) {
+         return num1 + num2;
+     }
+ 
+     let result = add(5,6)
+
+     console.log(result);*/
+     
+ 
---
+++ night.js
@@ -0,0 +1,107 @@
+// // function goodNight() {
+// //     console.log("hey you man");
+// // }
+// // goodNight();
+// // // goodNight();
+// // // goodNight();
+// // // goodNight();
+// // // goodNight();
+
+
+// // function hello(x){
+// //     console.log(`hello ${x}`);
+// // }
+// // hello("amaka");
+// // hello("chidi");
+// // hello("diana");
+// // hello("tobi");
+// // hello("kuku");
+
+// // let variable = "sweet" 
+// // hello(variable);
+
+
+// // function myHead(name, value) {
+// //     console.log(`you are lovely ${name}, ${value}`);
+// // }
+
+// // myHead("diana", 20)
+
+// function sweetGirl(age, height, bestFood)
+// {
+// let variable = `${age}, ${height}, ${bestFood}`
+// console.log(variable);
+// }
+// sweetGirl(29, 6.9, "rice");
+// sweetGirl(10, 70);
+// sweetGirl();
+// sweetGirl("cool", "okay");
+
+
+
+// function greet1(name) {
+//         return `Hello ${name}`
+// }
+
+// function greet2(name) {
+//     console.log(`Hello ${name}`)
+// }
+
+
+// let res1 = greet1("nameee")
+
+// let res2 = greet2("nameee")
+
+// console.log(res1)
+
+// console.log(res2)
+
+
+
+
+
+
+// let variable = true
+
+// let x = "diana"
+
+// let y = 25
+
+// let greet = 67.8
+
+// let person = { 
+//     eyecolor: "red",
+//     haircolor: "green",
+
+// }
+// console.log(person.eyecolor, person.haircolor);
+
+
+// function divide(num1, num2) {
+//     if(num2 === undefined) {
+//         return null
+//     }
+  
+//     return num1 / num2;
+// }
+// console.log(divide(10));
+
+
+
+function divide(num1, num2) {
+    if (num2 === undefined && num1 === undefined){
+        return null;
+
+    } else if (num1 === undefined || num2 === undefined){
+         return null;
+
+     } else if (num2 === 0){
+            return null;  
+     }
+     
+    
+    return num1 / num2;
+
+}
+console.log(divide(5))
+console.log(5)
